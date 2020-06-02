3c5ab45d4a58d7ee39e6bce60ddb9a413d800a53
Marie Antons <chefantons@gmail.com>
2020-06-02 13:28:15 -0500

    IInstance complete

--- DevelopingYou.Web/Services/HttpInstanceService.cs
+++ DevelopingYou.Web/Services/HttpInstanceService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DevelopingYou.Web.Services
@@ -10,5 +11,11 @@ namespace DevelopingYou.Web.Services
         //add private readonly httpclient
 
         //add http--service
+        private readonly HttpClient client;
+
+        public HttpInstanceService(HttpClient client)
+        {
+            this.client = client;
+        }
     }
 }

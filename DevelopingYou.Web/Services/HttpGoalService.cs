1dd762e9f35db6b80a32bddf98ed620f48e37cd8
cnickels21 <cnickels_21@yahoo.com>
2020-06-02 14:47:17 -0500

    goal service get route

--- DevelopingYou.Web/Services/HttpGoalService.cs
+++ DevelopingYou.Web/Services/HttpGoalService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using DevelopingYou.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DevelopingYou.Web.Services
@@ -8,7 +11,21 @@ namespace DevelopingYou.Web.Services
     public class HttpGoalService : IGoalService
     {
         //add private readonly httpclient
+        private readonly HttpClient client;
+
+        public HttpGoalService(HttpClient client)
+        {
+            this.client = client;
+        }
 
         //add http--service
+        public async Task<List<Goal>> GetGoals()
+        {
+            var responseStream = await client.GetStreamAsync("Goals");
+
+            List<Goal> result = await JsonSerializer.DeserializeAsync<List<Goal>>(responseStream);
+
+            return result;
+        }
     }
 }
--- DevelopingYou.Web/Services/IGoalService.cs
+++ DevelopingYou.Web/Services/IGoalService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevelopingYou.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace DevelopingYou.Web.Services
 {
     public interface IGoalService
     {
-        Task GetGoals();
+        Task<List<Goal>> GetGoals();
     }
 }
